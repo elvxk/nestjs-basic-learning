a6620d34a15ac6ab73d133f7ecf326e1342a38bc
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2024-12-02 22:24:36 +0700

    Feat/services (#2)
    * update(app.module): add project provider
    
    * feat(project/interface): add project interface
    
    * update(controller/projects): remove project controller spec, update projects controller
    
    * feat(service/projects): add project services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsController } from './projects/projects.controller';
+import { ProjectsService } from './projects/projects.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProjectsController],
-  providers: [AppService],
+  providers: [AppService, ProjectsService],
 })
 export class AppModule {}
