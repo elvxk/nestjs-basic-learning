1d5bc6980a0e177ca4f619df406e9a4ecaa992ff
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2024-12-03 21:19:05 +0700

    Feat/prisma (#3)
    * feat(prisma): add prisma to project
    
    * feat(prisma): add prisma module and service
    
    * feat(env): add env example
    
    * feat(prisma): import prisma module
    
    * update(interface/project): update project interface, change created_at and updated_at to Date type
    
    * update(lock): update bun lock file
    
    * update(service/projects): get all data project with prisma

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,30 +1,16 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Project } from './interfaces/projects.interface';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProjectsService {
-  // private readonly projects: Project[] = [];
+  constructor(private readonly prisma: PrismaService) {}
 
   async findAll(): Promise<Project[]> {
     try {
-      const res = await fetch('https://api.sandri.my.id/api/projects');
-      if (!res.ok) throw new Error(`Failed to fetch data: ${res.statusText}`);
-
-      const { data } = await res.json();
+      const data = await this.prisma.projects.findMany();
       if (!data) throw new Error('Data not found');
-
-      const allProjects: Project[] = data.map((data: Project) => ({
-        id: data.id,
-        title: data.title,
-        desc: data.desc,
-        stack: data.stack,
-        demo: data.demo,
-        image: data.image,
-        createdAt: data.createdAt,
-        updatedAt: data.updatedAt,
-      }));
-
-      return allProjects;
+      return data;
     } catch (error) {
       throw new HttpException(
         {
