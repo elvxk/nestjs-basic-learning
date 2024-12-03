25b8c95c6cfb78bdaad18f776b75917a9ee1f2a9
elvxk <pradiptaramadan9c@gmail.com>
2024-12-03 22:59:40 +0700

    feat(projects): add controller and service for create new project

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,12 +1,15 @@
 import {
+  Body,
   Controller,
   Get,
   HttpException,
   HttpStatus,
+  Post,
   Res,
 } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { Response } from 'express';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Controller('projects')
 export class ProjectsController {
@@ -16,10 +19,41 @@ export class ProjectsController {
   async findAll(@Res() res: Response) {
     try {
       const projects = await this.projectService.findAll();
-      res.status(HttpStatus.OK).send({
+
+      if (!projects || projects.length === 0) {
+        throw new HttpException('Data not found', HttpStatus.NOT_FOUND);
+      }
+
+      return res.status(HttpStatus.OK).send({
         statusCode: HttpStatus.OK,
         message: 'success get all projects',
         data: projects,
+      });
+    } catch (error) {
+      if (error instanceof HttpException) {
+        return res.status(error.getStatus()).send(error.getResponse());
+      }
+
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Internal server error',
+        error: error.message || 'Unknown error occurred',
+      });
+    }
+  }
+
+  @Post()
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const newProject = await this.projectService.create(createProjectDto);
+
+      return res.status(HttpStatus.CREATED).send({
+        statusCode: HttpStatus.CREATED,
+        message: 'success create project',
+        data: newProject,
       });
     } catch (error) {
       if (error instanceof HttpException) {
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,25 +1,17 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Project } from './interfaces/projects.interface';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProjectsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findAll(): Promise<Project[]> {
-    try {
-      const data = await this.prisma.projects.findMany();
-      if (!data) throw new Error('Data not found');
-      return data;
-    } catch (error) {
-      throw new HttpException(
-        {
-          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-          message: 'Internal server error',
-          error: error.message || 'Unknown error occurred',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  async findAll() {
+    return this.prisma.projects.findMany();
+  }
+
+  async create(data: Prisma.ProjectsCreateInput): Promise<Project> {
+    return this.prisma.projects.create({ data });
   }
 }
