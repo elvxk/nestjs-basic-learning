e1286b853133747f95af2ab31a1810792f592aba
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2024-12-04 21:17:24 +0700

    update(main): add cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.enableCors({
+    origin: ['*'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
