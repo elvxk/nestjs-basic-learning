d5b7e64d91478e8a02d22885b53c961b50b04eb5
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2024-12-04 18:26:37 +0700

    update(module/app): new import
    remove controller and provider,  import projects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProjectsController } from './projects/projects.controller';
-import { ProjectsService } from './projects/projects.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController, ProjectsController],
-  providers: [AppService, ProjectsService],
+  imports: [PrismaModule, ProjectsModule],
 })
 export class AppModule {}
