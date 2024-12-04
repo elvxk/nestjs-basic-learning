85d5368f2afab5f2aca9791db3fb966b4d268397
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2024-12-04 18:30:40 +0700

    feat(controller/project): delete by id
    add new endpoint for delete method

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,15 +1,18 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpException,
   HttpStatus,
+  NotFoundException,
   Post,
   Res,
 } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { Response } from 'express';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { DeleteProjectDto } from './dto/delete-project.dto';
 
 @Controller('projects')
 export class ProjectsController {
@@ -58,6 +61,39 @@ export class ProjectsController {
     } catch (error) {
       if (error instanceof HttpException) {
         return res.status(error.getStatus()).send(error.getResponse());
+      }
+
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Internal server error',
+        error: error.message || 'Unknown error occurred',
+      });
+    }
+  }
+
+  @Delete()
+  async delete(
+    @Body() deleteProjectDto: DeleteProjectDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const deleteProject = await this.projectService.deleteById(
+        deleteProjectDto.id,
+      );
+      return res.status(HttpStatus.OK).send({
+        message: `Project with ID ${deleteProjectDto.id} successfully deleted`,
+        statusCode: HttpStatus.OK,
+        data: deleteProject,
+      });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(
+          `Project with ID ${deleteProjectDto.id} not found`,
+        );
+      }
+
+      if (error instanceof HttpException) {
+        throw error;
       }
 
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
